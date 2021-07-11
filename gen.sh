65f03f3a1747d39b078e2fe165e93d0d93660935
tracer-oscpu2021 <tracer@oscpu.org>
2021-07-11 17:49:53 +0800

    ZhangJie HuangHongjing 20210230 20210300    Static hostname: amax-X299-MICRO-2          Icon name: computer-desktop            Chassis: desktop         Machine ID: d92791e3a4ba48a7bf1f71ee6e470792            Boot ID: 15c356d629934d39994e368049bf7afa   Operating System: Ubuntu 18.04.5 LTS             Kernel: Linux 5.4.0-72-generic       Architecture: x86-64  17:49:53 up  2:42,  3 users,  load average: 0.27, 0.24, 0.22

--- gen.sh
+++ gen.sh
@@ -1,7 +1,63 @@
 #!/bin/bash
 
-cd chisel
-sbt 'runMain top.elaborateTop'
-cp Verilog/Top.v ../examples/Top
-cd ../examples
-cd Top
+
+SHELL_PATH=$(dirname $(readlink -f "$0"))
+MYINFO_FILE=$SHELL_PATH"/myinfo.txt"
+
+ID=`sed '/^ID=/!d;s/.*=//' $MYINFO_FILE`
+NAME=`sed '/^Name=/!d;s/.*=//' $MYINFO_FILE`
+if [[ ! $ID ]] || [[ ! $NAME ]]; then
+    echo "Please fill your information in myinfo.txt!!!"
+    exit 1
+fi
+ID="${ID##*\r}"
+NAME="${NAME##*\r}"
+
+
+EXAMPLES_SRC_FOLDER="examples"
+EMU_FILE="emu"
+BUILD_FOLDER="build"
+
+BUILD="false"
+SIMULATE="false"
+CHECK_WAVE="false"
+V_TOP_FILE="top.v"
+CFLAGS=
+
+
+while getopts 'bt:sw:e:' OPT; do
+    case $OPT in
+        b) BUILD="true";;
+		e) EXAMPLES_PATH="$OPTARG";;
+        t) V_TOP_FILE="$OPTARG";;
+        s) SIMULATE="true";;
+        w) CHECK_WAVE="true"; WAVE_FILE="$OPTARG";;
+    esac
+done
+
+SRC_PATH=$SHELL_PATH/$EXAMPLES_SRC_FOLDER/$EXAMPLES_PATH
+BUILD_PATH=$SRC_PATH"/build"
+
+if [[ "$BUILD" == "true" ]]; then
+	echo $SRC_PATH
+	echo $BUILD_PATH
+
+	cd $SRC_PATH
+	CPP_SRC=`find . -maxdepth 1 -name "*.cpp"`
+	verilator  --cc --exe -o $EMU_FILE --trace $CFLAGS -Mdir ./$BUILD_FOLDER --build *.v $CPP_SRC
+	if [ $? -ne 0 ]; then
+        echo "Failed to run verilator!!!"
+        exit 1
+    fi
+	cd $SHELL_PATH
+	git add . -A --ignore-errors
+	(echo $NAME && echo $ID && hostnamectl && uptime) | git commit -F - -q --author='tracer-oscpu2021 <tracer@oscpu.org>' --no-verify --allow-empty 1>/dev/null 2>&1
+    sync
+fi
+
+
+# cd chisel
+# sbt 'runMain top.elaborateTop'
+# cp Verilog/Top.v ../examples/Top
+# cd ../examples
+# cd Top
