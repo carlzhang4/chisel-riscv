b535dc481cda9c9d8c6344d59011ce866cd97357
tracer-oscpu2021 <tracer@oscpu.org>
2021-07-13 17:26:01 +0800

    ZhangJie HuangHongjing 20210230 20210300    Static hostname: amax-X299-MICRO-2          Icon name: computer-desktop            Chassis: desktop         Machine ID: d92791e3a4ba48a7bf1f71ee6e470792            Boot ID: 15c356d629934d39994e368049bf7afa   Operating System: Ubuntu 18.04.5 LTS             Kernel: Linux 5.4.0-72-generic       Architecture: x86-64  17:26:01 up 2 days,  2:18,  5 users,  load average: 1.91, 1.45, 1.03

--- chisel/Verilog/Top.v
+++ chisel/Verilog/Top.v
@@ -1,36 +1,9 @@
-module If(
-  input  [31:0] io_test_in,
-  output [31:0] io_test_out
-);
-  wire [34:0] _io_test_out_T = io_test_in * 3'h5; // @[If_stage.scala 10:35]
-  assign io_test_out = _io_test_out_T[31:0]; // @[If_stage.scala 10:21]
-endmodule
-module Id(
-  input  [31:0] io_test_in,
-  output [31:0] io_test_out
-);
-  wire [32:0] _io_test_out_T = io_test_in * 1'h1; // @[Id_stage.scala 10:35]
-  assign io_test_out = _io_test_out_T[31:0]; // @[Id_stage.scala 10:21]
-endmodule
 module Top(
   input         clock,
   input         reset,
-  input  [31:0] io_test_in,
-  output [31:0] io_test_out
+  input         io_start,
+  input         io_wr_en,
+  input  [9:0]  io_wr_addr,
+  input  [31:0] io_wr_data
 );
-  wire [31:0] m_if_io_test_in; // @[Top.scala 11:26]
-  wire [31:0] m_if_io_test_out; // @[Top.scala 11:26]
-  wire [31:0] m_id_io_test_in; // @[Top.scala 12:26]
-  wire [31:0] m_id_io_test_out; // @[Top.scala 12:26]
-  If m_if ( // @[Top.scala 11:26]
-    .io_test_in(m_if_io_test_in),
-    .io_test_out(m_if_io_test_out)
-  );
-  Id m_id ( // @[Top.scala 12:26]
-    .io_test_in(m_id_io_test_in),
-    .io_test_out(m_id_io_test_out)
-  );
-  assign io_test_out = m_id_io_test_out; // @[Top.scala 15:21]
-  assign m_if_io_test_in = io_test_in; // @[Top.scala 13:25]
-  assign m_id_io_test_in = m_if_io_test_out; // @[Top.scala 14:25]
 endmodule
--- modules/Top/test.cpp
+++ modules/Top/test.cpp
@@ -6,12 +6,50 @@
 #include <fstream>
 #include "VTop.h"
 
+using namespace std;
 
 static VerilatedVcdC* tfp;
 
 static VTop* dut;
 
+
+
+int inst_rom[65536];
+
+void read_inst( char* filename){
+  FILE *fp = fopen(filename, "rb");
+  if( fp == NULL ) {
+		printf( "Can not open this file!\n" );
+		exit(1);
+  }
+  
+  fseek(fp, 0, SEEK_END);
+  size_t size = ftell(fp);
+  fseek(fp, 0, SEEK_SET);
+  size = fread(inst_rom, size, 1, fp);
+  fclose(fp);
+}
+
+void write_inst(int num){
+	for(int i=0;i<num;i++){
+		dut->clock = !dut->clock;
+		dut->eval();
+	  	tfp->dump(i*2);	
+		dut->clock = !dut->clock;
+		dut->io_wr_en = 1;
+		dut->io_wr_addr++;
+		dut->io_wr_data = inst_rom[i];		
+	  	dut->eval();
+	  	tfp->dump(i*2+1);		  		
+	}
+}
+
 int main(int argc, char **argv){
+	char filename[100];
+	printf("Please enter your filename~\n");
+	cin >> filename;
+	read_inst(filename);
+
 	Verilated::commandArgs(argc, argv);
   	Verilated::traceEverOn(true);
 
@@ -22,15 +60,25 @@ int main(int argc, char **argv){
 
 	dut->reset = 0;
 	dut->clock = 0;
-	dut->io_test_in = 0;
-	for(int i=0;i<500;i++){
+	dut->io_start = 0;
+	dut->io_wr_en = 0;
+	dut->io_wr_addr = -1;
+	dut->io_wr_data = 0;
+	int inst_num = 3;
+	write_inst(inst_num);
+	dut->io_start = 1;
+	for(int i=inst_num;i<20;i++){
 		dut->clock = !dut->clock;
-		dut->io_test_in++;
 		dut->eval();
-		tfp->dump(i);
+		tfp->dump(i*2);
+
+		dut->clock = !dut->clock;
+		dut->eval();
+		tfp->dump(i*2+1);
 	}
 	tfp->close();
 	delete tfp;
-	delete dut;
+	delete dut; 
+	exit(0);
 	return 0;
 }
