b2858ee0bc68074fde7e81431a3231b43e931a02
tracer-oscpu2021 <tracer@oscpu.org>
2021-07-15 20:29:38 +0800

    ZhangJie HuangHongjing 20210230 20210300    Static hostname: amax-X299-MICRO-2          Icon name: computer-desktop            Chassis: desktop         Machine ID: d92791e3a4ba48a7bf1f71ee6e470792            Boot ID: 15c356d629934d39994e368049bf7afa   Operating System: Ubuntu 18.04.5 LTS             Kernel: Linux 5.4.0-72-generic       Architecture: x86-64  20:29:38 up 4 days,  5:22,  5 users,  load average: 2.62, 1.79, 1.47

--- chisel/Verilog/Hello.v
+++ chisel/Verilog/Hello.v
@@ -1,9 +1,25 @@
 module Hello(
-  input         clock,
-  input         reset,
-  input  [31:0] io_in,
-  output [31:0] io_out
+  input        clock,
+  input        reset,
+  input  [7:0] io_x,
+  input  [7:0] io_y,
+  output [7:0] io_out
 );
-  wire [33:0] _io_out_T = io_in * 2'h2; // @[Hello.scala 12:22]
-  assign io_out = _io_out_T[31:0]; // @[Hello.scala 12:15]
+  wire [7:0] _io_out_T = io_x; // @[Hello.scala 17:23]
+  wire [262:0] _GEN_0 = {{255{_io_out_T[7]}},_io_out_T}; // @[Hello.scala 17:30]
+  wire [262:0] _io_out_T_2 = $signed(_GEN_0) << io_y; // @[Hello.scala 17:39]
+  assign io_out = _io_out_T_2[7:0]; // @[Hello.scala 17:15]
+  always @(posedge clock) begin
+    `ifndef SYNTHESIS
+    `ifdef PRINTF_COND
+      if (`PRINTF_COND) begin
+    `endif
+        if (~reset) begin
+          $fwrite(32'h80000002,"%x\n",5'hf); // @[Hello.scala 22:15]
+        end
+    `ifdef PRINTF_COND
+      end
+    `endif
+    `endif // SYNTHESIS
+  end
 endmodule
