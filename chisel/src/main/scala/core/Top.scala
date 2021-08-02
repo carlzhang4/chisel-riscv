f8c0b70c6a6a5e08f82199b2df64b590dc43b3fb
tracer-oscpu2021 <tracer@oscpu.org>
2021-08-02 22:45:30 +0800

    ZhangJie HuangHongjing 20210230 20210300    Static hostname: amax-X299-MICRO-2          Icon name: computer-desktop            Chassis: desktop         Machine ID: d92791e3a4ba48a7bf1f71ee6e470792            Boot ID: 62dffc71790b472ea0dd0de419c7dbe9   Operating System: Ubuntu 18.04.5 LTS             Kernel: Linux 5.4.0-72-generic       Architecture: x86-64  22:45:30 up 4 days, 10:39,  7 users,  load average: 4.89, 7.18, 8.39

--- chisel/src/main/scala/core/Top.scala
+++ chisel/src/main/scala/core/Top.scala
@@ -95,7 +95,7 @@ class Top(XLEN:Int) extends Module{
 
 	commit.io.valid := RegNext(RegNext(m_mem.io.wb_en_r))
 	commit.io.pc := RegNext(RegNext(RegNext(RegNext(RegNext(RegNext(RegNext(m_if.io.pc)))))))
-	commit.io.instr := RegNext(RegNext(RegNext(RegNext(RegNext(m_id.io.inst)))))
+	commit.io.instr := DontCare
 	commit.io.skip := false.B
 	commit.io.isRVC := false.B
 	commit.io.scFailed := false.B
